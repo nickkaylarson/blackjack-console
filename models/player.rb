d5960f19e602cf7e46e1dfacd24c9916360b0671
nickkaylarson <nicholaskaylarson@gmail.com>
2021-09-03 09:02:10 -0500

    add Player model, cards_to_s method

---
+++ models/player.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Player
+  attr_reader :name, :cards
+
+  def initialize(name)
+    @name = name
+    @cards = []
+    @bank = 100
+  end
+
+  def cards=(card)
+    @cards << card
+  end
+
+  def cards_to_s
+    cards = []
+    @cards.each do |card|
+        cards << "#{card.value.to_s} #{card.suit}"
+    end
+    cards
+  end
+end
