5a57627701ffc65733b3756149c81282c0c35e36
nickkaylarson <nicholaskaylarson@gmail.com>
2021-09-03 09:04:25 -0500

    create Game model, add create_players methods

--- game.rb
+++ game.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
+require 'tty-prompt'
+
+require_relative('./models/player')
+require_relative('./models/card')
+
 class Game
+def initialize
+    @players = []
+    @bank = 0
+end
+
   def start
     @prompt = TTY::Prompt.new(track_history: false)
     game_loop
@@ -8,5 +18,24 @@ class Game
 
   private
 
-  def game_loop; end
+  def create_dealer
+    dealer = Player.new('Dealer')
+    2.times { dealer.cards = Card.new }
+    dealer
+  end
+
+  def create_player
+    player = Player.new(@prompt.ask('Enter your name:'))
+    2.times { player.cards = Card.new }
+    player
+  end
+
+
+
+  def game_loop
+    loop do
+    @players << create_dealer
+    @players << create_player
+    end
+  end
 end
