d56641a527b157da36de5fc4abc90e2ba1657b45
nickkaylarson <nicholaskaylarson@gmail.com>
2021-09-12 07:24:04 -0500

    refactor

--- game.rb
+++ game.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative('./models/player')
-require_relative('./models/card')
+require_relative './models/player'
+require_relative './models/card'
 require_relative 'interface'
 
 class Game
