b7a39f541597fcdeaee08064af962418d93b89a4
nickkaylarson <nicholaskaylarson@gmail.com>
2021-09-27 04:43:36 -0500

    remove @points

--- models/hand.rb
+++ models/hand.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class Hand
-  attr_accessor :points
   attr_reader :cards
 
   def initialize
     @cards = []
-    @points = 0
   end
 
   def cards=(card)
@@ -22,14 +20,15 @@ class Hand
   end
 
   def calculate_points
-    @points = 0
+    points = 0
     @cards.each do |card|
-      @points += if card.value.to_i.zero?
-                   card.value == 'Ace' ? 1 : 10
-                 else
-                   card.value.to_i
-                 end
-      @points += 10 if card.value == 'Ace' && (@points + 10) <= 21
+      points += if card.value.to_i.zero?
+                  card.value == 'Ace' ? 1 : 10
+                else
+                  card.value.to_i
+                end
+      points += 10 if card.value == 'Ace' && (points + 10) <= 21
     end
+    points
   end
 end
