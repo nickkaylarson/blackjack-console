31da1edb37a9e657e1970dad71de7013698f474c
nickkaylarson <nicholaskaylarson@gmail.com>
2021-10-08 05:14:26 -0500

    store full deck inst. of use infinite deck

--- models/card.rb
+++ models/card.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-require_relative 'deck'
-
 class Card
   attr_reader :suit, :value
 
-  def initialize
-    @suit, @value = Deck.new.random_card
+  def initialize(deck)
+    @suit, @value = deck.card
   end
 end
